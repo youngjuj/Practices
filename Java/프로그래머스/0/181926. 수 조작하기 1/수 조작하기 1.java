836f35c6da0d21f9ce686961234dfdfd377cbaac
youngjuj <86656545+youngjuj@users.noreply.github.com>
2024-04-02 14:16:28 +0900

    [level 0] Title: 수 조작하기 1, Time: 1.82 ms, Memory: 86.3 MB -BaekjoonHub

--- Java/프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ Java/프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -5,18 +5,10 @@ class Solution {
         
         for(int i = 0; i < control.length(); i++){
             switch(control.charAt(i)){
-                case 'w':
-                    answer++;
-                    break;
-                case 's':
-                    answer--;
-                    break;
-                case 'd':
-                    answer += 10;
-                    break;
-                default:
-                    answer -= 10;
-                    break;
+                case 'w': answer += 1; break;
+                case 's': answer -= 1; break;
+                case 'd': answer += 10; break;
+                default : answer -= 10; break;
             }
         }
         return answer;

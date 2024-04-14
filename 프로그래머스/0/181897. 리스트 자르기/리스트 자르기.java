fdb4d288a3b427c5f51a5d9ac8406bb16ae6d4c8
youngjuj <86656545+youngjuj@users.noreply.github.com>
2024-04-14 22:12:46 +0900

    [level 0] Title: 리스트 자르기, Time: 0.02 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -1,32 +1,15 @@
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
         int[] answer = {};
-        int a = slicer[0], b = slicer[1], c = 1;
-        String log = "";
-        String[] log_arr = {};
+        int a = n == 1 ? 0 : slicer[0];
+        int b = n == 2 ? num_list.length -1 : slicer[1];
+        int c = n == 4 ? slicer[2] : 1;
         
-        switch( n ){
-            case 1:
-                a = 0; break;
-            case 2:
-                b = num_list.length - 1; break;
-            case 4:
-                c = slicer[2]; break;
+        answer = new int[(b-a)/c + 1];
+        
+        for(int i = 0, j = a; j <= b; j+= c){
+            answer[i++] = num_list[j];
         }
-        
-        answer = new int[(b-a)/c +1];
-
-        
-        for(int j = a; j <= b; j+= c){
-            log += num_list[j]+",";
-        }
-        
-        log_arr = log.split(",");
-        
-        for(int i = 0; i < answer.length; i++){
-            answer[i] = Integer.parseInt(log_arr[i]);
-        }
-        
         
         return answer;
     }
